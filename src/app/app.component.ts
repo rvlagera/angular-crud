42db22ab942850ab058510a848527f00ad9f46ad
Rommel Lagera <rommcomp@yahoo.com>
2022-08-24 21:56:04 +0800

    initial commit 25:39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularCrud';
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog(): void {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+    });
+  }
 }
